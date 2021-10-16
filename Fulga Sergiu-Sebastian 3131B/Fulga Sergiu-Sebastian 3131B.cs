c924d596e50e9c3bbe10f9b305c50f886c237217
Sebastian Fulga <sebastianfulga96@gmail.com>
2021-10-16 15:41:21 +0300

    AdaugareTemaLaborator2

---
+++ Fulga Sergiu-Sebastian 3131B/Fulga Sergiu-Sebastian 3131B.cs
@@ -0,0 +1,225 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.Drawing;
+using System.Threading;
+
+namespace Fulga_Sergiu_Sebastian_3131B
+{
+    // tema EGC - laborator 2 
+    class SimpleWindow3D : GameWindow
+    {
+        const float rotation_speed = 180.0f;
+        float angle;
+        bool showCube = true;
+        KeyboardState lastKeyPress;
+        MouseState originalMouseState;
+
+        // Constructor implicit
+        public SimpleWindow3D() : base(800, 600)
+        {
+            VSync = VSyncMode.On;
+
+            // utilizare eveniment pentru apasarea unei taste 
+            KeyDown += Keyboard_KeyDown;
+        }
+
+        // trateaza evenimentul generat de apasarea unei taste 
+        void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.Exit();
+            }
+
+            if (e.Key == Key.F11)
+            {
+                if (this.WindowState == WindowState.Fullscreen)
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Fullscreen;
+                }
+            }
+        }
+
+        // setare mediu OpenGL si incarcarea resurselor, daca este necesar 
+        // acest cod se executa inainte de desenarea efectiva a scenei 3D 
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(Color.MidnightBlue);
+            GL.Enable(EnableCap.DepthTest);
+        }
+
+        // se initiaza afisarea si se seteaza viewport-ul grafic 
+        // metoda va fi invocata la redimensionare 
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
+
+            double aspect_ratio = Width / (double)Height;
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)aspect_ratio, 1, 64);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspective);
+        }
+
+        // sectiunea pentru game logic / business logic 
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            KeyboardState keyboard = OpenTK.Input.Keyboard.GetState();
+            MouseState mouse = OpenTK.Input.Mouse.GetState();
+
+            // Se utilizeaza mecanismul de control input oferit de OpenTK 
+            if (keyboard[OpenTK.Input.Key.Escape])
+            {
+                Exit();
+                return;
+            }
+            // modificare pentru tema din laboratorul 2, afisarea cubului prin apasarea a doua taste 
+            else if (keyboard[OpenTK.Input.Key.P] && keyboard[Key.O] && !keyboard.Equals(lastKeyPress))
+            {
+                // ascunde comanda, prin apasarea a doua taste - cu verificare de remananenta ! 
+                if (showCube == true)
+                {
+                    showCube = false;
+                }
+                else
+                {
+                    showCube = true;
+                }
+            }
+            lastKeyPress = keyboard;
+
+            // modificare pentru tema din laboratorul 2 - cu verificare de remanenta 
+            if (!mouse.Equals(originalMouseState))
+            {
+                // Ascundere comandată, prin clic de mouse - fără testare remanență.
+                if (showCube == true)
+                {
+                    showCube = false;
+                }
+                else
+                {
+                    showCube = true;
+                }
+            }
+            originalMouseState = mouse;
+        }
+
+        // sectiune pentru randarea scenei 3D 
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Matrix4 lookat = Matrix4.LookAt(15, 50, 15, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+
+            // modificare pentru tema EGC - laborator 2
+            // aici se va roti cubul 
+            angle += rotation_speed * (float)e.Time;
+            GL.Rotate(angle, 0.0f, 1.0f, 0.0f);
+
+            // Exportăm controlul randării obiectelor către o metodă externă (modularizare).
+            if (showCube == true)
+            {
+                DrawCube();
+                DrawAxes_OLD();
+            }
+
+            SwapBuffers();
+            // Thread.Sleep(1);
+        }
+
+        private void DrawAxes_OLD()
+        {
+            GL.Begin(PrimitiveType.Lines);
+
+            // X
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(20, 0, 0);
+
+            // Y
+            GL.Color3(Color.Blue);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 20, 0);
+
+            // Z
+            GL.Color3(Color.Yellow);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, 20);
+
+            GL.End();
+        }
+
+        // Utilizăm modul imediat!!!
+        private void DrawCube()
+        {
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.Color3(Color.Silver);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+
+            GL.Color3(Color.Honeydew);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+
+            GL.Color3(Color.Moccasin);
+
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.IndianRed);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+
+            GL.Color3(Color.PaleVioletRed);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.ForestGreen);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+
+            GL.End();
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            // utilizarea cuvantului-cheie "using" va permite dealocarea memoriei o data ce obiectul nu mai este in uz 
+            using (SimpleWindow3D example = new SimpleWindow3D())
+            {
+                example.Run(30.0, 0.0);
+            }
+        }
+    }
+}
